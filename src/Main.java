b04460ca02b86d6f9c4c5c0a3b8f4bd54ed00262
Sergei Golendukhin <golendukhin.s.001@gmail.com>
2023-07-12 17:24:30 +0500

    data added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 21486; //цена за билет
+        int x = 20; //количество рублей для одной бонусной мили
+
+        System.out.println((price / x) + " миль/и будет начисленно за покупку");
+
+
+    }
+}
